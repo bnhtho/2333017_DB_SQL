8330a48af92889fcab2a53a122100e8522670a42
thohnb <thohnb@gmail.com>
2024-11-16 01:53:23 +0700

    Update trigger and add producre file

---
+++ BTL_2_Nhom1_L12_Producre.sql
@@ -0,0 +1,41 @@
+﻿use Medical
+-- Phần 2.4.1
+-- Câu 1
+-- Create PROCEDURE KiemTraNhanVienSanPham
+CREATE PROCEDURE KiemTraNhanVienSanPham
+    @MaChiNhanh INT
+AS
+BEGIN
+    DECLARE @SoLuongNhanVien INT;
+    DECLARE @SoLuongSanPham INT;
+    
+    -- Kiểm tra tham số đầu vào
+    IF @MaChiNhanh IS NULL
+    BEGIN
+        PRINT 'Mã chi nhánh không hợp lệ.';
+        RETURN;
+    END
+	 -- Kiểm tra sự tồn tại của mã chi nhánh trong bảng ChiNhanh
+    IF NOT EXISTS (SELECT 1 FROM ChiNhanh WHERE MaChiNhanh = @MaChiNhanh)
+    BEGIN
+        PRINT 'Mã chi nhánh không tồn tại trong hệ thống.';
+        RETURN;
+    END
+    -- Lấy số lượng nhân viên và sản phẩm từ bảng ChiNhanh
+    SELECT @SoLuongNhanVien = SoLuongNhanVien, @SoLuongSanPham = SoLuongSanPham
+    FROM ChiNhanh
+    WHERE MaChiNhanh = @MaChiNhanh;
+
+    -- Kiểm tra điều kiện số lượng nhân viên
+    IF @SoLuongNhanVien < @SoLuongSanPham / 10
+    BEGIN
+        PRINT 'Số lượng nhân viên không đủ để quản lý số lượng sản phẩm.';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Số lượng nhân viên đủ để quản lý số lượng sản phẩm.';
+    END
+END;
+GO
+EXEC KiemTraNhanVienSanPham @MaChiNhanh = 5;
+GO
--- BTL_2_Nhom1_L12_Triggers.sql
+++ BTL_2_Nhom1_L12_Triggers.sql
@@ -14,26 +14,3 @@ BEGIN
     END
 END;
 GO
-
-CREATE TRIGGER trg_CalculateTongTien
-ON ChiTietHoaDon
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    -- Tính toán lại tổng tiền cho mỗi bản ghi mới chèn hoặc cập nhật
-    UPDATE cthd
-    SET cthd.TongTien = 
-        CASE 
-            WHEN i.ApDungKhuyenMai = 1 THEN
-                (SELECT dh.TienThuoc FROM DonHang dh WHERE dh.MaDonHang = i.MaHoaDon)
-                - ISNULL(i.SoTienKhuyenMai, 0) -- Sử dụng ISNULL để tránh giá trị NULL
-                + (SELECT ctdh.ChiPhiGiaoHang FROM ChiTietDonHang ctdh WHERE ctdh.MaVanDon = i.MaHoaDon)
-            ELSE
-                (SELECT dh.TienThuoc FROM DonHang dh WHERE dh.MaDonHang = i.MaHoaDon)
-                + (SELECT ctdh.ChiPhiGiaoHang FROM ChiTietDonHang ctdh WHERE ctdh.MaVanDon = i.MaHoaDon)
-        END
-    FROM ChiTietHoaDon cthd
-    INNER JOIN inserted i ON cthd.MaHoaDon = i.MaHoaDon
-    WHERE cthd.TongTien IS NULL OR cthd.TongTien = 0; -- Cập nhật khi TongTien chưa được tính
-END;
-GO
