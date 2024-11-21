ee666438d89466154a5e07afb7dfc8fc338ee2b9
thohnb <thohnb@gmail.com>
2024-11-21 18:54:40 +0700

    Update script 2.4.1

--- BTL_2_Nhom1_L12_Producre.sql
+++ BTL_2_Nhom1_L12_Producre.sql
@@ -1,35 +1,43 @@
-﻿use Medical
--- Phần 2.4.1
--- Câu 1 (Thọ làm)
--- Create PROCEDURE KiemTraNhanVienSanPham
-CREATE PROCEDURE KiemTraNhanVienSanPham
+﻿CREATE PROCEDURE KiemTraNhanVienSanPham
     @MaChiNhanh INT
 AS
 BEGIN
     DECLARE @SoLuongNhanVien INT;
     DECLARE @SoLuongSanPham INT;
-    
+    DECLARE @NhanVienToiThieu INT;
+
     -- Kiểm tra tham số đầu vào
     IF @MaChiNhanh IS NULL
     BEGIN
         PRINT 'Mã chi nhánh không hợp lệ.';
         RETURN;
     END
-	 -- Kiểm tra sự tồn tại của mã chi nhánh trong bảng ChiNhanh
+
+    -- Kiểm tra sự tồn tại của mã chi nhánh trong bảng ChiNhanh
     IF NOT EXISTS (SELECT 1 FROM ChiNhanh WHERE MaChiNhanh = @MaChiNhanh)
     BEGIN
         PRINT 'Mã chi nhánh không tồn tại trong hệ thống.';
         RETURN;
     END
+
     -- Lấy số lượng nhân viên và sản phẩm từ bảng ChiNhanh
     SELECT @SoLuongNhanVien = SoLuongNhanVien, @SoLuongSanPham = SoLuongSanPham
     FROM ChiNhanh
     WHERE MaChiNhanh = @MaChiNhanh;
 
+    -- Tính số lượng nhân viên tối thiểu dựa trên số lượng sản phẩm
+    IF @SoLuongSanPham < 100
+        SET @NhanVienToiThieu = CEILING(@SoLuongSanPham / 50.0); -- 1 nhân viên quản lý 50 sản phẩm
+    ELSE IF @SoLuongSanPham BETWEEN 100 AND 500
+        SET @NhanVienToiThieu = CEILING(@SoLuongSanPham / 100.0); -- 1 nhân viên quản lý 100 sản phẩm
+    ELSE
+        SET @NhanVienToiThieu = CEILING(@SoLuongSanPham / 150.0); -- 1 nhân viên quản lý 150 sản phẩm
+
     -- Kiểm tra điều kiện số lượng nhân viên
-    IF @SoLuongNhanVien < @SoLuongSanPham / 100
+    IF @SoLuongNhanVien < @NhanVienToiThieu
     BEGIN
         PRINT 'Số lượng nhân viên không đủ để quản lý số lượng sản phẩm.';
+        PRINT 'Yêu cầu tối thiểu: ' + CAST(@NhanVienToiThieu AS VARCHAR) + ' nhân viên.';
     END
     ELSE
     BEGIN
@@ -37,8 +45,9 @@ BEGIN
     END
 END;
 GO
-EXEC KiemTraNhanVienSanPham @MaChiNhanh = 5;
-GO
+
+EXEC KiemTraNhanVienSanPham @MaChiNhanh = 1;
+
 
 -- Produre 2.4.1 do Đình Hoàn làm
 -- PROCEDURE kiểm tra số lượng nhân viên và sản phẩm.
