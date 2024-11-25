4390c92edc02fb2b2f3acf80ad3d7b85c6b33c1d
thohnb <thohnb@gmail.com>
2024-11-26 06:20:03 +0700

    Update 25/11/2024 fixes

--- BTL_2_Nhom_1_L12_16_Tables_Final_commit.sql
+++ BTL_2_Nhom_1_L12_16_Tables_Final_commit.sql
@@ -1,5 +1,4 @@
-﻿
-
+﻿-- Tạo database Medical nếu chưa tồn tại
 IF NOT EXISTS (
 	SELECT *
 	FROM sys.databases
@@ -10,11 +9,14 @@ BEGIN
 END
 GO
 
--- Hỗ trợ tiếng Việt có dấu trong Database Medical
+-- Hỗ trợ tiếng Việt có dấu trong database Medical
 ALTER DATABASE Medical
 COLLATE Latin1_General_100_CI_AS_SC_UTF8;
---
-USE Medical
+GO
+
+-- Sử dụng database Medical
+USE Medical;
+GO
 
 -- Phần 1: Chi nhánh, chương trình khuyến mãi và nhân viên
 -- [1] Chi nhánh
@@ -40,11 +42,12 @@ CREATE TABLE NhanVien (
 	LuongNhanVien DECIMAL(10, 2),
 	MaChiNhanh INT,
 	LoaiNV BIT,
-	ChungChi NVARCHAR(1),
-	ChuyenMon NVARCHAR(1),
+	ChungChi NVARCHAR(15),
+	ChuyenMon NVARCHAR(15),
 	GiayPhepHanhNghe NVARCHAR(8),
 	TrinhDo NVARCHAR(1),
-	CONSTRAINT	fk_nhanvien_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh)
+	CONSTRAINT fk_nhanvien_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh),
+	TinhTrang NVARCHAR(99) CHECK (TinhTrang IN ('Đã nghỉ việc', 'Đang làm việc'))
 );
 
 -- [3] Người phụ thuộc
@@ -55,14 +58,14 @@ CREATE TABLE NguoiPhuThuoc (
 	NgaySinh DATE NOT NULL CHECK (NgaySinh < GETDATE()),
 	GioiTinh CHAR(1) CHECK (GioiTinh IN ('M', 'F')),
 	MoiQuanHe NVARCHAR(99) CHECK (MoiQuanHe IN ('Anh', 'Chị', 'Em', 'Cha', 'Mẹ', 'Con')),
-	CONSTRAINT	fk_nguoiphuthuoc_nhanvien FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien)
+	CONSTRAINT fk_nguoiphuthuoc_nhanvien FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien)
 );
 
 -- [4] Khuyến mãi
 CREATE TABLE KhuyenMai (
 	MaKhuyenMai NVARCHAR(20) PRIMARY KEY,
 	MaChiNhanh INT,
-	CONSTRAINT	fk_khuyenmai_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh)
+	CONSTRAINT fk_khuyenmai_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh)
 );
 
 -- [5] Chương trình khuyến mãi
@@ -70,8 +73,8 @@ CREATE TABLE ChuongTrinhKhuyenMai (
 	MaKhuyenMai NVARCHAR(20) PRIMARY KEY,
 	NgayBatDau DATE,
 	NgayKetThuc DATE,
-	MoTa NVARCHAR(50),
-	LoaiGiam INT CHECK (LoaiGiam IN (1, 2)),
+	MoTa NVARCHAR(255),
+	LoaiGiam NVARCHAR(10) CHECK (LoaiGiam IN ('Phần trăm', 'Số tiền')),
 	GiaTriGiam DECIMAL(10, 2)
 );
 
@@ -119,7 +122,6 @@ CREATE TABLE KhachHang (
 );
 
 -- [10] Khách hàng và số điện thoại
--- Sửa lại kiểu dữ liệu SoDienThoai trong KhachHangSDT
 CREATE TABLE KhachHangSDT (
 	MaKhachHang NVARCHAR(10),
 	SoDienThoai NVARCHAR(11),
@@ -127,44 +129,57 @@ CREATE TABLE KhachHangSDT (
 	PRIMARY KEY (MaKhachHang, SoDienThoai)
 );
 
-
 -- [11] Đơn hàng
 CREATE TABLE DonHang (
 	MaDonHang NVARCHAR(10) PRIMARY KEY,
 	ToaThuoc NVARCHAR(255),
-	TienThuoc DECIMAL(10, 2) NOT NULL,
+	TienThuoc DECIMAL(10, 2),
 	Ngay DATE,
 	NhanXet NVARCHAR(500),
 	MaKhachHang NVARCHAR(10),
 	MaNhanVien INT,
+	-- Thêm trạng thái đơn hàng: Đã / chưa thanh toán
+	TrangThaiDonHang NVARCHAR(99) NOT NULL 
+    CHECK (TrangThaiDonHang IN ('Chưa thanh toán', 'Đã thanh toán')) ,
 	CONSTRAINT fk_donhang_makh FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
-	CONSTRAINT fk_donhang_manv FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien)
+	CONSTRAINT fk_donhang_manv FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien),
+
+	MaKhuyenMai NVARCHAR(20),
+	CONSTRAINT fk_donhang_chuongtrinhkhuyenmai FOREIGN KEY (MaKhuyenMai) REFERENCES ChuongTrinhKhuyenMai(MaKhuyenMai)
 );
 
 -- [12] Đơn vị giao hàng
 CREATE TABLE DonViGiaoHang (
 	MaDonVi NVARCHAR(10) PRIMARY KEY,
-	TenDonVi NVARCHAR(255) NOT NULL,
+	TenDonVi NVARCHAR(255) NOT NULL
 );
 
 -- Phần 4: Các mối liên hệ giữa các thực thể
 -- [13] Chi tiết đơn hàng
 CREATE TABLE ChiTietDonHang (
-	MaVanDon NVARCHAR(10) PRIMARY KEY,
-	TinhTrang NVARCHAR(50),
+	MaVanDon NVARCHAR(10),
+	-- Đơn hàng đã được giao bởi nhà cung cấp -- mối quan hệ : Được giao bởi
+	TinhTrang NVARCHAR(50)  NOT NULL CHECK (TinhTrang IN ('Đã giao', 'Đang giao', 'Đã huỷ')),
 	DiaChiDonHang NVARCHAR(255) NOT NULL,
 	MaDonViVanChuyen NVARCHAR(10),
 	ChiPhiGiaoHang DECIMAL(10, 2),
-	CONSTRAINT fk_chitiet_donhang_don_vi_van_chuyen FOREIGN KEY (MaDonViVanChuyen) REFERENCES DonViGiaoHang(MaDonVi)
-);
+	CONSTRAINT fk_chitiet_donhang_don_vi_van_chuyen FOREIGN KEY (MaDonViVanChuyen) REFERENCES DonViGiaoHang(MaDonVi),
+	-- Update ngày 25/11 : Thêm ràng buộc khoá ngoại chi tiết đơn hàng với chương trình khuyến mãi
+
+	MaDonHang NVARCHAR(10),
+	CONSTRAINT fk_chitietdonhang_donhang FOREIGN KEY (MaDonHang) REFERENCES DonHang(MaDonHang)
+);    -- cái này là mối quan hệ Được giao bởi giữa Đơn hàng và Đơn vị giao hàng
+
 
 -- [14] Chi tiết hóa đơn
 CREATE TABLE ChiTietHoaDon (
-	MaHoaDon NVARCHAR(10) PRIMARY KEY,
+	MaHoaDon NVARCHAR(10),
 	SoTienKhuyenMai DECIMAL(10, 2) DEFAULT 0,
-	ApDungKhuyenMai BIT NOT NULL DEFAULT 0,
 	ThoiGianXuatHoaDon DATETIME NOT NULL,
-	TongTien DECIMAL(10, 2) NOT NULL
+	TongTien DECIMAL(10, 2),
+	MaDonHang NVARCHAR(10),
+	PRIMARY KEY (MaHoaDon, MaDonHang),
+	CONSTRAINT fk_hoadon_donhang FOREIGN KEY (MaDonHang) REFERENCES DonHang(MaDonHang)
 );
 
 -- [15] Thanh toán
@@ -172,11 +187,12 @@ CREATE TABLE ThanhToan (
 	MaDonHang NVARCHAR(10),
 	MaChiNhanh INT,
 	MaKhachHang NVARCHAR(10),
-	CONSTRAINT  fk_thanhtoan_ma_donhang FOREIGN KEY (MaDonHang) REFERENCES DonHang(MaDonHang),
-	CONSTRAINT  fk_thanhtoan_ma_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh),
-	CONSTRAINT  fk_thanhtoan_ma_khachhang FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
+	CONSTRAINT fk_thanhtoan_ma_donhang FOREIGN KEY (MaDonHang) REFERENCES DonHang(MaDonHang),
+	CONSTRAINT fk_thanhtoan_ma_chinhanh FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh),
+	CONSTRAINT fk_thanhtoan_ma_khachhang FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
 	PRIMARY KEY (MaDonHang)
 );
+
 -- [16] Bảng bao gồm
 CREATE TABLE BaoGom (
 	MaSanPham NVARCHAR(10),
@@ -186,7 +202,3 @@ CREATE TABLE BaoGom (
 	CONSTRAINT fk_baogom_ma_donhang FOREIGN KEY (MaDonHang) REFERENCES DonHang(MaDonHang),
 	PRIMARY KEY (MaSanPham, MaDonHang)
 );
-
--- -- Cập nhật khóa ngoại MaNhanVienQuanLy sau khi insert
-ALTER TABLE ChiNhanh
-ADD CONSTRAINT FK_chinhanh_nhanvien FOREIGN KEY (MaNhanVienQuanLy) REFERENCES NhanVien(MaNhanVien);
