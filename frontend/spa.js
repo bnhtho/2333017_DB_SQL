0be09d32b2eb42672090388c3c5ae4d8ba3694bf
thohnb <thohnb@gmail.com>
2024-12-01 08:29:34 +0700

    Add frontend

--- frontend/spa.js
+++ frontend/spa.js
@@ -1,73 +1,72 @@
-$(document).ready(function() {
-  // Hàm tải trang nhân viên (insert_employee.html)
-  function loadInsertEmployeePage() {
+$(document).ready(function () {
+  // Các hàm tải trang
+  function loadPage(url) {
     $.ajax({
-      url: 'insert_employee.html',  // Tải trang nhân viên
+      url: url,
       method: 'GET',
-      success: function(data) {
-        $('#content').html(data);  // Chèn nội dung vào #content
-        registerEvents();  // Đăng ký lại sự kiện sau khi tải nội dung
-      }
-    });
-  }
-  function loadCommentList() {
-    $.ajax({
-      url: 'comment.html',  // Tải trang nhân viên
-      method: 'GET',
-      success: function(data) {
-        $('#content').html(data);  // Chèn nội dung vào #content
-        registerEvents();  // Đăng ký lại sự kiện sau khi tải nội dung
-      }
-    });
-  }
-  // Hàm tải trang danh sách nhân viên (employee_list.html)
-  function loadEmployeeList() {
-    $.ajax({
-      url: 'employee_list.html',  // Tải trang danh sách nhân viên
-      method: 'GET',
-      success: function(data) {
-        $('#content').html(data);  // Chèn nội dung vào #content
-        registerEvents();  // Đăng ký lại sự kiện sau khi tải nội dung
+      success: function (data) {
+        $('#content').html(data);
+        registerEvents();
       }
     });
   }
 
-
-  function loadproductLink() {
-    $.ajax({
-      url: 'product.html',  // Tải trang danh sách sản phẩm
-      method: 'GET',
-      success: function(data) {
-        $('#content').html(data);  // Chèn nội dung vào #content
-        registerEvents();  // Đăng ký lại sự kiện sau khi tải nội dung
-      }
-    });
-  }
-
-  // Hàm đăng ký lại các sự kiện (sự kiện cho các liên kết)
+  // Hàm đăng ký lại các sự kiện
   function registerEvents() {
-    // Sự kiện nhấn vào "Nhấn vào đây để xem danh sách nhân viên"
-    $('#loadEmployeeList').on('click', function(e) {
-      e.preventDefault();  // Ngừng hành động mặc định
-      loadEmployeeList();  // Tải danh sách nhân viên
+    $('#loadEmployeeList').on('click', function (e) {
+      e.preventDefault();
+      window.location.hash = 'employeeList'; // Thay đổi hash
+      loadPage('employee_list.html');
     });
 
-    // Sự kiện quay lại trang nhập nhân viên
-    $('#backToInsertEmployeePage').on('click', function(e) {
+    $('#backToInsertEmployeePage').on('click', function (e) {
       e.preventDefault();
-      loadInsertEmployeePage();  // Tải lại trang nhập nhân viên
+      window.location.hash = 'insertEmployee'; // Thay đổi hash
+      loadPage('insert_employee.html');
     });
-    // Load danh sách sản phẩm
-    $('#productLink').on('click', function(e) {
+
+    $('#productLink').on('click', function (e) {
       e.preventDefault();
-      loadproductLink();  // Tải lại trang nhập nhân viên
+      window.location.hash = 'product'; // Thay đổi hash
+      loadPage('product.html');
     });
-    // Load danh sách đánh giá
-    $('#loadCommentsPage').on('click',function(e){
+
+    $('#loadCommentsPage').on('click', function (e) {
       e.preventDefault();
-      loadCommentList(); // Tải trang load bình luận
-    })
+      window.location.hash = 'comments'; // Thay đổi hash
+      loadPage('comment.html');
+    });
+
+    $('#loadhotProduct').on('click', function (e) {
+      e.preventDefault();
+      window.location.hash = 'hotProduct'; // Thay đổi hash
+      loadPage('trending_products.html');
+    });
   }
-  loadInsertEmployeePage();  // Tải trang nhân viên khi bắt đầu
 
+  // Xử lý hash để tải trang phù hợp
+  function loadInitialPage() {
+    const hash = window.location.hash.substring(1); // Bỏ dấu #
+    switch (hash) {
+      case 'employeeList':
+        loadPage('employee_list.html');
+        break;
+      case 'product':
+        loadPage('product.html');
+        break;
+      case 'comments':
+        loadPage('comment.html');
+        break;
+      case 'hotProduct':
+        loadPage('trending_products.html');
+        break;
+      case 'insertEmployee':
+      default:
+        loadPage('insert_employee.html');
+        break;
+    }
+  }
+
+  // Gọi hàm khi bắt đầu
+  loadInitialPage();
 });
